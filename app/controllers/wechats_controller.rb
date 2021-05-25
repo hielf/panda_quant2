aeaf3b1c19bb8046b17d309ab69517f0ab006112
moumou <hielf1984@gmail.com>
2021-05-25 22:44:26 +0800

    Update wechats_controller.rb

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -66,14 +66,11 @@ class WechatsController < ApplicationController
     # package = Package.find_by(package_type: subscription.package_type)
     user_stock_list = user.stock_lists
     rest_watch_num = subscription.nil? ? 0 : (subscription.watch_num - user_stock_list.count)
-    wechat.custom_message_send Wechat::Message.to(openid).text("您当前使用的套餐为：
-      #{package_type}\n已订阅数量为：
-      #{user_stock_list.count}\n剩余可订阅数量：
-      #{rest_watch_num}")
-    wechat.custom_message_send Wechat::Message.to(openid).text("请回复下列序号：")
-    wechat.custom_message_send Wechat::Message.to(openid).text("1）继续订阅\n2）查询当前订阅列表\n3）删除订阅")
+    wechat.custom_message_send Wechat::Message.to(openid).text("您当前使用的套餐：#{package_type}\n已订阅数量：#{user_stock_list.count}\n剩余可订阅数量：#{rest_watch_num}")
+    wechat.custom_message_send Wechat::Message.to(openid).text("请回复下列序号操作：\n1）继续订阅\n2）查询当前订阅列表\n3）删除订阅")
 
-    request.reply.text "User: #{request[:FromUserName]} click #{key}"
+    # request.reply.text "User: #{request[:FromUserName]} click #{key}"
+    request.reply.success
   end
 
   # When user click the menu button
