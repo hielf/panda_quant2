d4bdec897b34a1741ab40601d259316bb41df755
moumou <hielf1984@gmail.com>
2021-06-22 23:03:10 +0800

    Update Package.js

--- app/javascript/components/Package/Package.js
+++ app/javascript/components/Package/Package.js
@@ -94,7 +94,6 @@ const Package = (props) => {
     .then(resp => {
       if (resp.data.status != 0) {
         alert("用户未验证")
-        location.replace(`/subscribtion/${package_id}`)
         // props.history.push('/package/success')
       }
       console.log(resp.data)
@@ -116,12 +115,13 @@ const Package = (props) => {
           },
           function(res){
             if(res.err_msg == "get_brand_wcpay_request:ok" ){
-              alert("支付成功");
+              location.replace(`/subscribtion/${package_id}`)
               // that.props.history.push(RouterList.private);
               // 使用以上方式判断前端返回,微信团队郑重提示：
               //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
             }else{
               alert("支付失败,请重试");
+              location.replace(`/subscribtion/${package_id}`)
             }
           }
         );
