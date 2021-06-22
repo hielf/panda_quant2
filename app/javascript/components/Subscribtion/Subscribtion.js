2a40bcae2a0e7bfc539c9beff2665ec43c7296ae
moumou <hielf1984@gmail.com>
2021-06-22 23:57:11 +0800

    Update Subscribtion.js

--- app/javascript/components/Subscribtion/Subscribtion.js
+++ app/javascript/components/Subscribtion/Subscribtion.js
@@ -10,23 +10,33 @@ const Wrapper = styled.div`
 
 const Column = styled.div`
   background: #fff;
-  /* line-height: 95vh; */
 `
 
 const Main = styled.div`
   display: table;
   padding: 0;
-  height: 95vh;  
+  height: 95vh;
+  width: 100%;
+  font-family: MicrosoftYaHei;
   /* height: 50vh; */
+`
+
+const Title = styled.div`
+  color: #ffd658;
+  font-size: 22px;
+  font-weight: light;
+  /* display: table-cell; */
+  vertical-align: middle;
+  padding: 20vh 0;
 `
 
 const Message = styled.span`
   color: #fec107;
   font-size: 20px;
   font-weight: light;
-  font-family: MicrosoftYaHei;
-  display: table-cell;
+  /* display: table-cell; */
   vertical-align: middle;
+  padding: 10px 0;
 `
 
 const Subscribtion = (props) => {
@@ -47,7 +57,8 @@ const Subscribtion = (props) => {
       <Fragment>
         <Column>
           <Main>
-            <Message>套餐购买成功，请在公众号内回复"3"，设置您的订阅</Message>
+            <Title>套餐购买成功!</Title>
+            <Message>请在公众号内回复"3"，设置您的订阅</Message>
           </Main>
         </Column>
       </Fragment>
