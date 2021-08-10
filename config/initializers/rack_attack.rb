813fbcb2d357d0c283c097b69667e4bc5782712b
moumou <hielf1984@gmail.com>
2021-08-10 11:47:28 +0800

    Update rack_attack.rb

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -5,7 +5,19 @@ class Rack::Attack
   #   [ 200, { 'Content-Type' => 'application/json' }, [{ status: 0, message: "Too many requests!!!", error: "Too many requests!!!" }.to_json] ]
   # end
 
-  throttle('wechat api', limit: 1, period: 1.seconds) do |req|
+  Rack::Attack.throttled_callback = lambda do |request|
+    # NB: you have access to the name and other data about the matched throttle
+    #  request.env['rack.attack.matched'],
+    #  request.env['rack.attack.match_type'],
+    #  request.env['rack.attack.match_data'],
+    #  request.env['rack.attack.match_discriminator']
+
+    # Using 503 because it may make attacker think that they have successfully
+    # DOSed the site. Rack::Attack returns 429 for throttling by default
+    [ 429, {}, ["Server Error\n"]]
+  end
+
+  Rack::Attack.throttle('wechat api', limit: 1, period: 1.seconds) do |req|
     if req.path == '/wechat' && req.post?
       req.ip
     end
